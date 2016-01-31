da47b105946c25a1372a774e1cc30092a84b3c66
mylifelineadam <adam@mylifeline.org>
2016-01-30 20:45:51 -0700

    views home header show bootstrap again.

--- application/views/home/inc/header_view.php
+++ application/views/home/inc/header_view.php
@@ -3,15 +3,11 @@
     <head>
         <title>Test</title>
     
-        <?php /*
         <link rel="stylesheet" href="<?=base_url();?>public/css/bootstrap.min.css" />
-        */ ?>
         <link rel="stylesheet" href="<?=base_url();?>public/css/style.css" />
 
-        <script href="<?=base_url();?>public/js/jquery.js" ></script>
-        <?php /*
-        <script href="<?=base_url();?>public/js/bootstrap.js" ></script>
-        */ ?>
+        <script src="<?=base_url();?>public/js/jquery.js" ></script>
+        <script src="<?=base_url();?>public/js/bootstrap.js" ></script>
     
     </head>
     <body>
@@ -19,7 +15,7 @@
         <header>
             JrDash
         </header>
-        
+
         <!-- start:wrapper -->
         <div class="wrapper">
             
