b6f66888c284598f3be9897cc40352a6265fbead
mylifelineadam <adam@mylifeline.org>
2016-02-01 08:46:58 -0700

    view home home_view comment out js for testing

--- application/views/home/home_view.php
+++ application/views/home/home_view.php
@@ -45,12 +45,13 @@
         var postData = $(this).serialize();
 
         $.post(url, postData, function(o) {
+            /*
             if (o.result == 1) {
                 alert('Login success.');
             } else {
                 alert('Invalid login');
             }
-
+            */
         }, 'json');
 
     });
