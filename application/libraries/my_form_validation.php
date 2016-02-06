a817237be8d69b74247aec580d6447a9050fd6b8
mylifelineadam <adam@mylifeline.org>
2016-02-06 15:46:29 -0700

    extend form_validation fixes

--- application/libraries/my_form_validation.php
+++ application/libraries/my_form_validation.php
@@ -2,7 +2,6 @@
 
 class MY_Form_validation extends CI_Form_valildation
 {
-
 	public function __construct( $config = array() )
 	{
 		parent::__construct($config);
@@ -11,9 +10,7 @@ class MY_Form_validation extends CI_Form_valildation
 	public function error_array()
 	{
 		if ( count($this->_error_array > 0) ) {
-
 			die("here i am my_form_validation.php");
-
 			return $this->_error_array;
 		}
 	}
