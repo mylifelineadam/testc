74b9f8de76a8584e27ef669f689a67d972c47a19
mylifelineadam <adam@mylifeline.org>
2016-02-06 13:26:46 -0700

    controllers user register function correct field rules

--- application/controllers/user.php
+++ application/controllers/user.php
@@ -80,12 +80,10 @@ class User extends CI_Controller
 
 		$this->form_validation->set_rules('first_name', 'First Name', 'required|min_length[2]|max_length[30]');
 		$this->form_validation->set_rules('last_name', 'Last Name', 'required|min_length[2]|max_length[30]');
-		$this->form_validation->set_rules('logi', 'Login', 'required|min_length[6]|max_length[16]|is_unique[user.login]');
 		$this->form_validation->set_rules('login', 'Login', 'required|min_length[6]|max_length[16]|is_unique[user.login]');
 		$this->form_validation->set_rules('city', 'City', 'exact_length[0]');
 		$this->form_validation->set_rules('email', 'Email', 'required|min_length[6]|valid_email|is_unique[user.email]|matches[email_again]');
 		$this->form_validation->set_rules('password', 'Password', 'required|min_length[6]|matches[password_again]');
-		$this->form_validation->set_rules('password_again', 'Password Again', 'required|min_length[6]|max_length[16]');
 
 		# if validation found errors then...
 		if ($this->form_validation->run() == FALSE ) {
