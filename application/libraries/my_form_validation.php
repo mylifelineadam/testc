3f170804e8314ac6922823cad0fd1813248d55bb
mylifelineadam <adam@mylifeline.org>
2016-02-06 16:06:19 -0700

    libraries my_form_validation testing

--- application/libraries/my_form_validation.php
+++ application/libraries/my_form_validation.php
@@ -10,7 +10,6 @@ class MY_Form_validation extends CI_Form_validation
 	public function error_array()
 	{
 		if ( count($this->_error_array > 0) ) {
-			die("here i am my_form_validation.php");
 			return $this->_error_array;
 		}
 	}
