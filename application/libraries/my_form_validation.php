6e860c92d436ed4209f86e073f10521874671d56
mylifelineadam <adam@mylifeline.org>
2016-02-06 16:11:01 -0700

    my_form_validation fixes

--- application/libraries/my_form_validation.php
+++ application/libraries/my_form_validation.php
@@ -2,16 +2,16 @@
 
 class MY_Form_validation extends CI_Form_validation
 {
-	public function __construct( $config = array() )
-	{
-		parent::__construct($config);
-	}
-
-	public function error_array()
-	{
-		if ( count($this->_error_array > 0) ) {
-			return $this->_error_array;
-		}
-	}
-
+    public function __construct($config = array())
+    {
+        parent::__construct($config);
+    }
+    
+    public function error_array()
+    {
+        if (count($this->_error_array > 0)) {
+            return $this->_error_array;
+        }
+    }
+    
 }
