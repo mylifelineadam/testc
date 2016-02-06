fea20ec0d6b327c14dba4e0f4240abf3825cc9f8
mylifelineadam <adam@mylifeline.org>
2016-02-06 16:12:50 -0700

    fixes

--- application/controllers/user.php
+++ application/controllers/user.php
@@ -104,8 +104,8 @@ class User extends CI_Controller
 			# set result to "0" and send back validation errors
 			$this->output->set_output(json_encode([
 				'result' => 0
-				# , 
-				# 'data' => $this->form_validation->error_array()
+				 , 
+				'data' => $this->form_validation->error_array()
 			]));
 
 			# die("here i am user.php");
