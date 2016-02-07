0f2670d5071e87d2c072928d8192b014f6fb2a66
mylifelineadam <adam@mylifeline.org>
2016-02-06 17:19:02 -0700

    form_validation extend testing

--- application/libraries/my_form_validation.php
+++ application/libraries/my_form_validation.php
@@ -8,12 +8,14 @@ class MY_Form_validation extends CI_Form_validation
         parent::__construct($config);
     }
 
+    /*
     public function error_array()
     {
         if (count($this->_error_array > 0)) {
             return $this->_error_array;
         }
     }
+    */
 
     public function test_get_five() {
         return '5';
