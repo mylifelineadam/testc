956e8694dc9a92ab0bb61dd47b23b785c2a669e0
mylifelineadam <adam@mylifeline.org>
2016-02-09 20:50:20 -0700

    libraries uncomment test

--- application/libraries/my_form_validation.php
+++ application/libraries/my_form_validation.php
@@ -8,14 +8,12 @@ class MY_Form_validation extends CI_Form_validation
         parent::__construct($config);
     }
 
-    /*
     public function error_array()
     {
         if (count($this->_error_array > 0)) {
             return $this->_error_array;
         }
     }
-    */
 
     public function test_get_five() {
         return '5';
