a661457e54bc6525b5b7dfbc97986ac21637c692
mylifelineadam <adam@mylifeline.org>
2016-02-13 14:22:37 -0700

    remove unnecessary function

--- application/libraries/My_User_agent.php
+++ application/libraries/My_User_agent.php
@@ -8,28 +8,5 @@ class MY_User_agent extends CI_User_agent {
     {
         parent::__construct($config);
     }
-
-    public function find_device_category()
-    {
-    	$temp_device_category = 0;
-
-	    $detect = new Mobile_Detect();
-	    if ( $detect->isTablet() ) {
-
-    		$temp_device_category = 3; # Tablet
-
-	    } elseif ( $detect->isMobile() ) {
-	        
-    		$temp_device_category = 2; # Tablet
-
-    	} else {
-    		
-    		$temp_device_category = 1; # Desktop
-    	}
-
-    	return $temp_device_category;
-
-    }
-
-
+    
 }
