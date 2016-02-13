c065eb336e2fe33884a096e1b0f344de2bf9e186
mylifelineadam <adam@mylifeline.org>
2016-02-13 13:03:19 -0700

    libraries My_Form_validation add restriction to the top of the file

--- application/libraries/MY_Form_validation.php
+++ application/libraries/MY_Form_validation.php
@@ -1,5 +1,7 @@
 <?php
 
+if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
 class MY_Form_validation extends CI_Form_validation
 {
     public function __construct($config = array())
@@ -13,5 +15,6 @@ class MY_Form_validation extends CI_Form_validation
             return $this->_error_array;
         }
     }
-    
+
+
 }
