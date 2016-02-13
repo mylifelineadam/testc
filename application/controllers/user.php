52787a9a7e87d001649ea044d91bf2a23ecf8b5b
mylifelineadam <adam@mylifeline.org>
2016-02-13 14:55:25 -0700

    test

--- application/controllers/user.php
+++ application/controllers/user.php
@@ -118,11 +118,13 @@ class User extends CI_Controller
 		$last_name = trim( $this->input->post('last_name') );
 		$email = trim( strtolower( $this->input->post('email') ) );
 		$password = trim( $this->input->post('password') );
+
+		die('no errors, let us continue');
+
 		
 		$register_user_agent = $this->agent->agent_string();
 
 		echo $register_user_agent;
-		die();
 
 		# $register_device_category = $this->agent->find_device_category();
 		$register_device_category = '';
