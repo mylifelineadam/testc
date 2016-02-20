cbceeba80dea3eda3bc363eb5d62cf1907df1dbd
mylifelineadam <adam@mylifeline.org>
2016-02-20 16:51:12 -0700

    mobile detect testing

--- application/libraries/mobiledetect.php
+++ application/libraries/mobiledetect.php
@@ -2,30 +2,40 @@
 
 require_once APPPATH.'third_party/mobile_detect/Mobile_Detect.php';
 
-class MobileDetect extends Mobile_Detect 
-{
-	public function __construct()
-	{
-	}
+class MobileDetect extends Mobile_Detect {
+
+    public function __construct() {
+        parent::__construct();
+    }
 
     public function find_device_category()
     {
-    	$temp_device_category = 0;
+ 
+        echo __LINE__."<br/>";
 
-	    $detect = $this->my_mobile_detect->Mobile_Detect();
-	    
-	    if ( $detect->isTablet() ) {
-    		$temp_device_category = 3; # Tablet
+        $temp_device_category = 0;
 
-	    } elseif ( $detect->isMobile() ) {
-    		$temp_device_category = 2; # Tablet
+        $detect = $this->Mobile_Detect();
 
-    	} else {
-    		$temp_device_category = 1; # Desktop
-    	}
+        echo __LINE__."<br/>";
 
-    	return $temp_device_category;
+        if ( $detect->isTablet() ) {
+            $temp_device_category = 3; # Tablet
+
+        } elseif ( $detect->isMobile() ) {
+            $temp_device_category = 2; # Tablet
+
+        } else {
+            $temp_device_category = 1; # Desktop
+        }
+
+        echo __LINE__."<br/>";
+
+        return $temp_device_category;
 
     }
 
+
+
 }
+
